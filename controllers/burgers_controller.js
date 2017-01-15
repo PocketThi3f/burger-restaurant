374dffe788ca3e6049f7daf2005f30dc7be9a1fe
PocketThi3f <krslai@yahoo.com>
2017-01-15 15:34:42 -0800

    changing up burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,32 @@
 // Here are all the functions that will route the app
 var express = require("express");
 var router = express.Router();
-var cat = require("../models/burgers.js");
+var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
 	res.redirect("/burgers");
+});
+
+router.get("/burgers", function(req, res) {
+	burger.selectAll(function(data) {
+		var hbsObject = { burgers: data };
+		console.log(hbsObject);
+		res.render("index", hbsObject);
+	});
+});
+
+router.post("/burgers/create", function(req, res) {
+	burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function() {
+		res.redirect("/burgers");
+	});
+});
+
+router.put("/burgers/update/:ID", function(req, res) {
+	var condition = "ID = "+ req.params.ID;
+
+	console.log("condition", condition);
+
+	cat.update({ burger_name: req.body.burger_name }, condition, function() {
+		res.redirect("/burgers");
+	});
 });
