a6a6da0ca2e242bdb4ef496b1efb4f0d0a60aaea
PocketThi3f <krslai@yahoo.com>
2017-01-15 20:29:23 -0800

    brushing up the kitchen...

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,36 @@
+// Here is the O.R.M. where the functions are written to take inputs and conditions
 var connection = require("../config/connection.js");
+
+function objToSql(ob) {
+	// column1=value, column2=value...
+	var arr = [];
+
+	for(var key in ob) {
+		if(ob.hasProperty(key)) {
+			arr.push(key + '=' + ob[key]);
+		}
+	}
+
+	return arr.toString();
+}
+
+var orm = {
+	selectAll: function(tableInput, cb) {
+		var queryString = "SELECT * FROM " + tableInput + ';';
+		connection.query(queryString, function(err, result) {
+
+		});
+	},
+
+		// vals is an array of values that we want to save into cols
+		// cols are the names of columns we want to insert values into
+	insertOne: function(table, cols, vals, cb) {
+		var queryString = "INSERT INTO " + table;
+
+		queryString += ' (';
+		queryString += cols.toString();
+		queryString += ') ';
+		queryString += 'VALUES (';
+		queryString += 
+	}
+}
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Big Mac", 0), 
-("Tak'n Bi Surprise", 0), ("Slobb'r Cheesy Festive", 0)
+INSERT INTO burgers (burger_name, devoured) VALUES ("Ter-into-my-yaki Booger", 0), 
+("Tak'n Bi Surprise", 0), ("Slobb'r Cheesy Festive", 0);
