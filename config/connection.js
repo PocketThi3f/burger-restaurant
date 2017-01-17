708bd5bf51ce6ebfca77eb850a83f54916b28f8e
PocketThi3f <krslai@yahoo.com>
2017-01-16 19:38:36 -0800

    preparing for pre-open

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ and the export made by the ORM */
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-	port: 1337,
+	PORT: 1337,
 	host: "localhost",
 	user: "root",
 	password: "password",
