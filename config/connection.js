40aabc43702010e9ee9888084a8c12d63ff561b2
PocketThi3f <krslai@yahoo.com>
2017-01-18 02:42:17 -0800

    mee

--- config/connection.js
+++ config/connection.js
@@ -6,13 +6,13 @@ var connection = mysql.createConnection({
 	port: 1337,
 	host: "localhost",
 	user: "root",
-	password: "",
+	password: "password",
 	database: "burgers_db"
 });
 
 connection.connect(function(err) {
 	if (err) {
-		console.error("Sorry, error connecting to the server " + err.stack);
+		console.error("Sorry, error " + err.stack);
 		return;
 	}
 	console.log("Connected as ID: " + connection.threadId);
