d63661e067f7f9ec67954c908b174b776e6b5e41
PocketThi3f <krslai@yahoo.com>
2017-01-18 12:09:30 -0800

    pepper

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
-var port = process.env.port || 1337;
+var port = process.env.port || 3000;
 app.listen(port, function() {
   console.log("I am now listening to port: " + port);
 });
