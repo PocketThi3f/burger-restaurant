841fdd1c5937d3a5619026a6adbe34b2ebe41e2d
PocketThi3f <krslai@yahoo.com>
2017-01-18 21:43:19 -0800

    sda

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,11 @@
 and the export made by the ORM */
 var mysql = require("mysql");
 var connection;
+console.log(process.env.JAWSDB_URL);
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log("I am Jaws");
 } else {
   connection = mysql.createConnection({
     host: "localhost",
@@ -12,6 +14,7 @@ if(process.env.JAWSDB_URL) {
     password: "password",
     database: "burgers_db"
   });
+  console.log("the issue");
 }
 
 connection.connect(function(err) {
